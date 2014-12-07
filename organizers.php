b7352ddb4fe4104ac196dc0ff1b00986802b28ff
Thulio Philipe <thulioph@gmail.com>
2014-12-07 03:35:45 -0300

    Adds content, structure and style of organizers page

--- organizers.php
+++ organizers.php
@@ -6,139 +6,178 @@
     <main id="main" class="main">
       <section id="organizers" class="row organizers"> <!-- Organizers -->
         <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1">
-          <h3 data-i18n="about.title" class="section-title">About</h3>
+          <h3 data-i18n="organizers.title" class="section-title">Organizers</h3>
         </div>
 
         <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1">
-          <aside id="skoll" class="organizer-box"> <!-- Skoll -->
-            <ul class="organizer-list">
-              <li class="organizer-item">
-                <figure class="organizer-image">
+          <aside id="skoll" class="organizers-box skoll"> <!-- Skoll -->
+            <ul class="organizers-list">
+              <li class="row organizers-item">
+                <figure class="col-xs-12 col-sm-4 col-md-3 organizers-image">
                   <img src="" alt="">
                 </figure>
 
-                <h4 class="organizer-name">Mark Smolinsk</h4>
-                <p class="organizer-description">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Vero nobis deserunt dolor modi quia error! Minus quas itaque
-                  reiciendis repudiandae excepturi molestias aperiam minima
-                  maxime debitis, et sit vero deleniti quod corporis magnam
-                  eaque suscipit possimus cum alias. Exercitationem, deleniti.
-                </p>
+                <div class="col-xs-12 col-sm-8 col-md-9 organizers-description">
+                  <h4 class="organizers-name">Mark Smolinsk</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Vero nobis deserunt dolor modi quia error! Minus quas itaque
+                    reiciendis repudiandae excepturi molestias aperiam minima
+                    maxime debitis, et sit vero deleniti quod corporis magnam
+                    eaque suscipit possimus cum alias. Exercitationem, deleniti.
+                  </p>
+
+                  <a href="#" class="web">meusite.com</a>
+                  <a href="http://www.skollglobalthreats.org" target="_blank" class="company skoll">skollglobalthreats.org</a>
+                </div>
               </li>
 
-              <li class="organizer-item">
-                <figure class="organizer-image">
+              <li class="row organizers-item">
+                <figure class="col-xs-12 col-sm-4 col-md-3 organizers-image">
                   <img src="" alt="">
                 </figure>
 
-                <h4 class="organizer-name">Jennifer Olsen</h4>
-                <p class="organizer-description">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Vero nobis deserunt dolor modi quia error! Minus quas itaque
-                  reiciendis repudiandae excepturi molestias aperiam minima
-                  maxime debitis, et sit vero deleniti quod corporis magnam
-                  eaque suscipit possimus cum alias. Exercitationem, deleniti.
-                </p>
+                <div class="col-xs-12 col-sm-8 col-md-9 organizers-description">
+                  <h4 class="organizers-name">Jennifer Olsen</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Vero nobis deserunt dolor modi quia error! Minus quas itaque
+                    reiciendis repudiandae excepturi molestias aperiam minima
+                    maxime debitis, et sit vero deleniti quod corporis magnam
+                    eaque suscipit possimus cum alias. Exercitationem, deleniti.
+                  </p>
+
+                  <a href="#" class="web">meusite.com</a>
+                  <a href="http://www.skollglobalthreats.org" target="_blank" class="company skoll">skollglobalthreats.org</a>
+                </div>
               </li>
 
-              <li class="organizer-item">
-                <figure class="organizer-image">
+              <li class="row organizers-item">
+                <figure class="col-xs-12 col-sm-4 col-md-3 organizers-image">
                   <img src="" alt="">
                 </figure>
 
-                <h4 class="organizer-name">Mario Libel</h4>
-                <p class="organizer-description">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Vero nobis deserunt dolor modi quia error! Minus quas itaque
-                  reiciendis repudiandae excepturi molestias aperiam minima
-                  maxime debitis, et sit vero deleniti quod corporis magnam
-                  eaque suscipit possimus cum alias. Exercitationem, deleniti.
-                </p>
+                <div class="col-xs-12 col-sm-8 col-md-9 organizers-description">
+                  <h4 class="organizers-name">Mario Libel</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Vero nobis deserunt dolor modi quia error! Minus quas itaque
+                    reiciendis repudiandae excepturi molestias aperiam minima
+                    maxime debitis, et sit vero deleniti quod corporis magnam
+                    eaque suscipit possimus cum alias. Exercitationem, deleniti.
+                  </p>
+
+                  <a href="#" class="web">meusite.com</a>
+                  <a href="http://www.skollglobalthreats.org" target="_blank" class="company skoll">skollglobalthreats.org</a>
+                </div>
               </li>
             </ul>
           </aside>
 
-          <aside id="epitrack" class="organizer-box"> <!-- Epitrack -->
-            <ul class="organizer-list">
-              <li class="organizer-item">
-                <figure class="organizer-image">
+          <aside id="epitrack" class="organizers-box epitrack"> <!-- Epitrack -->
+            <ul class="organizers-list">
+              <li class="row organizers-item">
+                <figure class="col-xs-12 col-sm-4 col-md-3 organizers-image">
                   <img src="" alt="">
                 </figure>
 
-                <h4 class="organizer-name">Onicio Leal</h4>
-                <p class="organizer-description">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Vero nobis deserunt dolor modi quia error! Minus quas itaque
-                  reiciendis repudiandae excepturi molestias aperiam minima
-                  maxime debitis, et sit vero deleniti quod corporis magnam
-                  eaque suscipit possimus cum alias. Exercitationem, deleniti.
-                </p>
+                <div class="col-xs-12 col-sm-8 col-md-9 organizers-description">
+                  <h4 class="organizers-name">Onicio Leal</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Vero nobis deserunt dolor modi quia error! Minus quas itaque
+                    reiciendis repudiandae excepturi molestias aperiam minima
+                    maxime debitis, et sit vero deleniti quod corporis magnam
+                    eaque suscipit possimus cum alias. Exercitationem, deleniti.
+                  </p>
+
+                  <a href="#" class="web">meusite.com</a>
+                  <a href="http://www.epitrack.com.br" target="_blank" class="company epitrack">epitrack.com.br</a>
+                </div>
               </li>
 
-              <li class="organizer-item">
-                <figure class="organizer-image">
+              <li class="row organizers-item">
+                <figure class="col-xs-12 col-sm-4 col-md-3 organizers-image">
                   <img src="" alt="">
                 </figure>
 
-                <h4 class="organizer-name">Juliana Perazzo</h4>
-                <p class="organizer-description">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Vero nobis deserunt dolor modi quia error! Minus quas itaque
-                  reiciendis repudiandae excepturi molestias aperiam minima
-                  maxime debitis, et sit vero deleniti quod corporis magnam
-                  eaque suscipit possimus cum alias. Exercitationem, deleniti.
-                </p>
+                <div class="col-xs-12 col-sm-8 col-md-9 organizers-description">
+                  <h4 class="organizers-name">Juliana Perazzo</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Vero nobis deserunt dolor modi quia error! Minus quas itaque
+                    reiciendis repudiandae excepturi molestias aperiam minima
+                    maxime debitis, et sit vero deleniti quod corporis magnam
+                    eaque suscipit possimus cum alias. Exercitationem, deleniti.
+                  </p>
+
+                  <a href="#" class="web">meusite.com</a>
+                  <a href="http://www.epitrack.com.br" target="_blank" class="company epitrack">epitrack.com.br</a>
+                </div>
               </li>
 
-              <li class="organizer-item">
-                <figure class="organizer-image">
+              <li class="row organizers-item">
+                <figure class="col-xs-12 col-sm-4 col-md-3 organizers-image">
                   <img src="" alt="">
                 </figure>
 
-                <h4 class="organizer-name">George Dimech</h4>
-                <p class="organizer-description">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Vero nobis deserunt dolor modi quia error! Minus quas itaque
-                  reiciendis repudiandae excepturi molestias aperiam minima
-                  maxime debitis, et sit vero deleniti quod corporis magnam
-                  eaque suscipit possimus cum alias. Exercitationem, deleniti.
-                </p>
+                <div class="col-xs-12 col-sm-8 col-md-9 organizers-description">
+                  <h4 class="organizers-name">George Dimech</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Vero nobis deserunt dolor modi quia error! Minus quas itaque
+                    reiciendis repudiandae excepturi molestias aperiam minima
+                    maxime debitis, et sit vero deleniti quod corporis magnam
+                    eaque suscipit possimus cum alias. Exercitationem, deleniti.
+                  </p>
+
+                  <a href="#" class="web">meusite.com</a>
+                  <a href="http://www.epitrack.com.br" target="_blank" class="company epitrack">epitrack.com.br</a>
+                </div>
               </li>
 
-              <li class="organizer-item">
-                <figure class="organizer-image">
+              <li class="row organizers-item">
+                <figure class="col-xs-12 col-sm-4 col-md-3 organizers-image">
                   <img src="" alt="">
                 </figure>
 
-                <h4 class="organizer-name">Jones Albuquerque</h4>
-                <p class="organizer-description">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Vero nobis deserunt dolor modi quia error! Minus quas itaque
-                  reiciendis repudiandae excepturi molestias aperiam minima
-                  maxime debitis, et sit vero deleniti quod corporis magnam
-                  eaque suscipit possimus cum alias. Exercitationem, deleniti.
-                </p>
+                <div class="col-xs-12 col-sm-8 col-md-9 organizers-description">
+                  <h4 class="organizers-name">Jones Albuquerque</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Vero nobis deserunt dolor modi quia error! Minus quas itaque
+                    reiciendis repudiandae excepturi molestias aperiam minima
+                    maxime debitis, et sit vero deleniti quod corporis magnam
+                    eaque suscipit possimus cum alias. Exercitationem, deleniti.
+                  </p>
+
+                  <a href="#" class="web">meusite.com</a>
+                  <a href="http://www.epitrack.com.br" target="_blank" class="company epitrack">epitrack.com.br</a>
+                </div>
               </li>
 
-              <li class="organizer-item">
-                <figure class="organizer-image">
+              <li class="row organizers-item">
+                <figure class="col-xs-12 col-sm-4 col-md-3 organizers-image">
                   <img src="" alt="">
                 </figure>
 
-                <h4 class="organizer-name">Renata Costa</h4>
-                <p class="organizer-description">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Vero nobis deserunt dolor modi quia error! Minus quas itaque
-                  reiciendis repudiandae excepturi molestias aperiam minima
-                  maxime debitis, et sit vero deleniti quod corporis magnam
-                  eaque suscipit possimus cum alias. Exercitationem, deleniti.
-                </p>
+                <div class="col-xs-12 col-sm-8 col-md-9 organizers-description">
+                  <h4 class="organizers-name">Renata Costa</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Vero nobis deserunt dolor modi quia error! Minus quas itaque
+                    reiciendis repudiandae excepturi molestias aperiam minima
+                    maxime debitis, et sit vero deleniti quod corporis magnam
+                    eaque suscipit possimus cum alias. Exercitationem, deleniti.
+                  </p>
+
+                  <a href="#" class="web">meusite.com</a>
+                  <a href="http://www.epitrack.com.br" target="_blank" class="company epitrack">epitrack.com.br</a>
+                </div>
               </li>
             </ul>
           </aside>
         </div>
-
       </section>
     </main>
 
