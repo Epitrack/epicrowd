325b7117016295f3b7257bbf15819c4825844dee
Thulio Philipe <thulioph@gmail.com>
2015-02-06 00:17:56 -0300

    Some adjusts to translate

--- index.php
+++ index.php
@@ -187,17 +187,17 @@
                 </div>
               </li>
 
-              <!--
+
               <li class="agenda-item">
                 <span class="hour" data-i18n="agenda.day03Hour02">9:45 am</span>
 
                 <div class="aditional wall-panel">
                   <h4 class="agenda-title" data-i18n="agenda.rodrigoAngeramiJoaoBoscoSiqueira">Global Health and the impact dissemination of infectious threats during mass gatherings.</h4>
-                  <span class="panelists" data-i18n="agenda.panelists">Panelists:</span> <p class="agenda-description" data-i18n="agenda.rodrigoAngeramiJoaoBoscoSiqueiraDescrption01">Rodrigo Angerami - UNICAMP (Brazil), João Bosco Siqueira - UFG (Brazil)</p>
+                  <span class="panelists" data-i18n="agenda.panelists">Panelists:</span> <p class="agenda-description" data-i18n="agenda.rodrigoAngeramiJoaoBoscoSiqueiraDescrption01">Rodrigo Angerami - UNICAMP (Brazil)</p>
                   <span class="moderator" data-i18n="agenda.moderator">Moderator:</span> <p class="agenda-description" data-i18n="agenda.rodrigoAngeramiJoaoBoscoSiqueiraDescrption02">Mary Wilson - Harvard University (USA)</p>
                 </div>
               </li>
-               -->
+
 
               <li class="agenda-item bottom">
                 <span class="hour" data-i18n="agenda.day03Hour03">11:00 am</span>
